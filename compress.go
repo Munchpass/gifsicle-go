a8586e477f88f791f43c8c717f5e93561570197a
Joseph Chen <jchen42703@gmail.com>
2024-07-09 12:45:48 -0400

    feat: created CompressFromReader

--- compress.go
+++ compress.go
@@ -39,3 +39,42 @@ func Compress(w io.Writer, g *gif.GIF, o *Options) error {
 
 	return gifsicleCli.InputGif(g).Output(w).Run()
 }
+
+/*
+Shortcut function to compress GIFs quickly and easily with gifsicle
+using an io.Reader like a file or buffer.
+
+Example:
+
+	testFilePath := path.Join("testfiles", "portrait_3mb.gif")
+	testFile, err := os.Open(testFilePath)
+	if err != nil {
+		return err
+	}
+
+	var buf bytes.Buffer
+	err = gifsicle.CompressFromReader(&buf, testFile, &gifsicle.Options{
+		Lossy:         200,
+		OptimizeLevel: gifsicle.OPTIMIZE_LEVEL_THREE,
+		NumColors:     256,
+	})
+	if err != nil {
+		return err
+	}
+*/
+func CompressFromReader(w io.Writer, r io.Reader, o *Options) error {
+	gifsicleCli, err := NewGifsicle()
+	if err != nil {
+		return fmt.Errorf("NewGifsicle failed: %v", err)
+	}
+
+	if o != nil {
+		gifsicleCli.Lossy(o.Lossy)
+		gifsicleCli.OptimizeLevel(o.OptimizeLevel)
+		if o.NumColors >= 2 {
+			gifsicleCli.NumColors(o.NumColors)
+		}
+	}
+
+	return gifsicleCli.Input(r).Output(w).Run()
+}
