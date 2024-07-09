d066902e818ddd5d53f0cd2a81ce1f86611a9bbc
Joseph Chen <jchen42703@gmail.com>
2024-07-09 12:46:12 -0400

    test: created TestCompressFromReader and write test output to GIF for compression tests

--- compress_test.go
+++ compress_test.go
@@ -2,14 +2,30 @@ package gifsicle_test
 
 import (
 	"bytes"
+	"fmt"
 	"image/gif"
 	"io"
 	"os"
 	"path"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/Munchpass/gifsicle-go"
 )
+
+func writeGifToOutput(t *testing.T, gifBytes []byte, origGifPath string) {
+	baseFname := filepath.Base(origGifPath)
+	ext := filepath.Ext(baseFname)
+	rawFname, _ := strings.CutSuffix(baseFname, ext)
+	outputGifPath := fmt.Sprintf("./testoutput/%s.gif", rawFname)
+
+	_ = os.Mkdir("./testoutput", os.FileMode(0777))
+	err := os.WriteFile(outputGifPath, gifBytes, os.FileMode(0777))
+	if err != nil {
+		t.Fatalf("failed to write output gif: %v\n", err)
+	}
+}
 
 func TestCompress(t *testing.T) {
 	testFilePath := path.Join("testfiles", "portrait_3mb.gif")
@@ -67,17 +83,25 @@ func TestCompress(t *testing.T) {
 		t.Fatalf("gifsicle.Compress: %v\n", err)
 	}
 
-	// Need to re-read to get the original size
-	testFile, err = os.Open(testFilePath)
+	writeGifToOutput(t, buf.Bytes(), testFilePath)
+}
+
+func TestCompressFromReader(t *testing.T) {
+	testFilePath := path.Join("testfiles", "portrait_3mb.gif")
+	testFile, err := os.Open(testFilePath)
 	if err != nil {
 		t.Fatalf("os.Open: %v\n", err)
 	}
 
-	rawSource, err = io.ReadAll(testFile)
+	var buf bytes.Buffer
+	err = gifsicle.CompressFromReader(&buf, testFile, &gifsicle.Options{
+		Lossy:         200,
+		OptimizeLevel: gifsicle.OPTIMIZE_LEVEL_THREE,
+		NumColors:     256,
+	})
 	if err != nil {
-		t.Fatalf("io.ReadAll: %v\n", err)
+		t.Fatalf("CompressFromReader: %v\n", err)
 	}
 
-	t.Logf("size before: %d kb, size after: %d kb\n",
-		len(rawSource)/1024, buf.Len()/1024)
+	t.Logf("CompressFromReader: compressed from 2.9mb to %d kb\n", buf.Len()/1024)
 }
